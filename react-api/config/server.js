026c00dbf7adb5ae1cd76c4ea8d7deadb578f38b
Thiago Queiroz Pirolla <thiago.pirolla@gmail.com>
2021-08-04 22:36:30 -0300

    initial commit

---
+++ react-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db489e0b165db05d3671c7fbee282b12'),
+    },
+  },
+});
---
+++ react-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a8f6fd25-0c77-4177-9d65-d0692f5abf77'
+};
